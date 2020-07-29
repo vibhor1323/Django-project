122ce10205fff5b6c250ebb328176c56b9e4eaae
vibhor1323 <vibhor2000.agrawal@gmail.com>
2020-07-30 01:14:20 +0530

    login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html') , name='login')
 
 ]
