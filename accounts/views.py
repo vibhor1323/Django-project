c9ae30cec0e3f379ec05d20343185d2a98c035a3
vibhor1323 <vibhor2000.agrawal@gmail.com>
2020-08-06 00:43:43 +0530

    adding registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render,redirect
+from accounts.forms import RegistrationForm
 
-# Create your views here.
 def home(request):
     numbers=[1,2,3,4,5]
     name= 'Vibhor Agrawal'
     args={'myName':name, 'numbers':numbers}
     return render(request, 'accounts/home.html',args)
+
+
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form= RegistrationForm()
+
+    args = {'form':form}
+    return render(request, 'accounts/reg_form.html',args)
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.conf.urls import url,include
+from tutorial import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/',include('accounts.urls')),
+    url(r'^$',views.login_redirect, name='login_redirect'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^account/',include('accounts.urls')),
 ]
